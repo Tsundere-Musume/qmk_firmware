7800f9e72b92ce5ba2999158ba06300cf31d3261
Tsundere-Musume <maharjanelvis55@gmail.com>
2026-04-01 13:06:54 -0500

    Silakka54 - new number layer

---
+++ keyboards/silakka54/keymaps/mine/config.h
@@ -0,0 +1,1 @@
+#define SPECULATIVE_HOLD
--- keyboards/silakka54/keymaps/mine/keymap.c
+++ keyboards/silakka54/keymaps/mine/keymap.c
@@ -12,20 +12,20 @@ enum layers {
 #define HRM_A    LGUI_T(KC_A)
 #define HRM_S    LT(SYM, KC_S)
 #define HRM_D    LSFT_T(KC_D)
-#define HRM_F    LCTL_T(KC_F)
+#define HRM_F    LT(NAV, KC_F)
 
-#define HRM_J    RCTL_T(KC_J)
+#define HRM_J    LT(NUM, KC_J)
 #define HRM_K    RSFT_T(KC_K)
 #define HRM_L    LT(SYM, KC_L)
 #define HRM_SCLN RGUI_T(KC_SCLN)
 
-#define HRM_V    LALT_T(KC_V)
-#define HRM_M    RALT_T(KC_M)
+#define HRM_V    LCTL_T(KC_V)
+#define HRM_M    RCTL_T(KC_M)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_ESC,  SELLINE, C(KC_V), C(KC_A), C(KC_C), KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_BSPC, HRM_A,   HRM_S,   HRM_D,   HRM_F,   KC_G,                        KC_H,    HRM_J,   HRM_K,   HRM_L,   HRM_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    HRM_V,   KC_B,                        KC_N,    HRM_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
@@ -49,11 +49,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NUM] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                            KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, QK_RBT,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_SLSH, KC_9   , KC_8   , KC_7   , KC_ASTR,                     KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_MINS, KC_3   , KC_2   , KC_1   , KC_PLUS,                     KC_TRNS, KC_LSFT , KC_LGUI, KC_LCTL, KC_LALT, KC_TRNS,
+        KC_TRNS, KC_X   , KC_6   , KC_5   , KC_4   , KC_PERC,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                            KC_TRNS, KC_0   , KC_TRNS,   KC_TRNS, QK_LLCK, KC_TRNS
     ),
 };
 // clang-format on
+
+bool get_speculative_hold(uint16_t keycode, keyrecord_t* record) {
+    return true;
+    // switch (keycode) { // These keys may be speculatively held.
+    //     case LCTL_T(KC_ESC):
+    //     case LSFT_T(KC_Z):
+    //     case RSFT_T(KC_SLSH):
+    //         return true;
+    // }
+    // return false; // Disable otherwise.
+}
+///////////////////////////////////////////////////////////////////////////////
+// Combos (https://docs.qmk.fm/features/combo)
+///////////////////////////////////////////////////////////////////////////////
+const uint16_t caps_combo[] PROGMEM = {KC_C, KC_COMM, COMBO_END};
+const uint16_t x_c_combo[] PROGMEM = {KC_X, KC_C, COMBO_END};
+// clang-format off
+combo_t key_combos[] = {
+    COMBO(caps_combo, CW_TOGG),          // J and , => activate Caps Word.
+    COMBO(x_c_combo, KC_BSLS),           // J and K => backslash
+    // COMBO(comm_dot_combo, KC_SCLN),      // , and . => ;
+    // COMBO(f_n_combo, OSL(FUN)),          // F and N => FUN layer
+};
